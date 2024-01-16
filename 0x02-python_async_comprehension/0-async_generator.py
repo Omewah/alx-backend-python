a0138fd071ae2d1b5fd0a5eb50e4942e8ac388d5
Omewah <joelomewah@ymail.com>
2024-01-16 18:47:31 +0100

    async comprehension with python

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random number after looping 10x"""
     for _ in range(10):
         await asyncio.sleep(1)
